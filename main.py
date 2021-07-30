cd700228ac1d41b31bff40e8879235faf4eb8877
thisaraMJ <madushantjg@gmail.com>
2021-07-30 13:11:19 +0530

    character recognizing and writing

--- main.py
+++ main.py
@@ -1,28 +1,27 @@
 import cv2 as cv
+import pytesseract
 
-image_file = 'data/img1.jpg'
+pytesseract.pytesseract.tesseract_cmd = 'C:\Program Files\Tesseract-OCR/tesseract.exe'
 
-img = cv.imread(image_file)
+img = cv.imread('data/img1.jpg')
+img = cv.cvtColor(img, cv.COLOR_BGR2RGB)
+# print(pytesseract.image_to_string(img))
 
-# cv.imshow("original image", img)
-# cv.waitKey(0)
-
-# invert image
-inverted_image = cv.bitwise_not(img)
-cv.imwrite('data/inverted.jpg', inverted_image)
+## Detecting Characters
+height, width, channels = img.shape
+boxes = pytesseract.image_to_boxes(img)
 
 
-# imgshow = cv.imread('data/inverted.jpg')
-# cv.imshow("original image", imgshow)
-# cv.waitKey(0)
+f = open("data/characters.txt", "a")
 
-# Binarization
 
-def grayscale(image):
-    return cv.cvtColor(image, cv.COLOR_BGR2GRAY)
-
-gray_image = grayscale(img)
-cv.imwrite("temp/gray.jpg", gray_image)
-
-thresh, image_bw = cv.threshold(gray_image, 127, 255, cv.THRESH_BINARY)
-cv.imwrite("temp/bw_image.jpg", image_bw)
+for b in boxes.splitlines():
+    b = b.split(' ')
+    print(b)
+    x, y, w, h = int(b[1]), int(b[2]), int(b[3]), int(b[4])
+    cv.rectangle(img, (x, height-y), (w, height-h), (0, 0, 255), 1)
+    # cv.putText(img, b[0], (x, height-y+25), cv.FONT_HERSHEY_COMPLEX, 1, (40, 40, 55), 1)
+    f.write(" "+b[0])
+f.close()
+cv.imshow('Result', img)
+cv.waitKey(0)
